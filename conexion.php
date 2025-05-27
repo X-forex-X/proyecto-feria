4bf5d3a9c5593aadffcea1c49a3b0f47404c2ca7
Janiip <168372737+Janiip@users.noreply.github.com>
2025-05-27 20:21:31 -0300

    cambios
    boton enviar en formulario

---
+++ conexion.php
@@ -0,0 +1,17 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "asistencia";
+
+// Crear conexión
+$con = new mysqli($servername, $username, $password, $database);
+
+// Comprobar conexión
+if ($con->connect_error) {
+    die("Conexión fallida: " . $con->connect_error);
+}
+echo "Conexión exitosa";
+
+
+?>
