5e997e1804804f077667e069917a56d30613c2d1
Janiip <168372737+Janiip@users.noreply.github.com>
2025-05-27 20:44:10 -0300

    conexion php
    funcional

--- conexion.php
+++ conexion.php
@@ -5,13 +5,19 @@ $password = "";
 $database = "asistencia";
 
 // Crear conexión
-$con = new mysqli($servername, $username, $password, $database);
+// $con = new mysqli($servername, $username, $password, $database);
 
-// Comprobar conexión
-if ($con->connect_error) {
-    die("Conexión fallida: " . $con->connect_error);
-}
-echo "Conexión exitosa";
+// // Comprobar conexión
+// if ($con->connect_error) {
+//     die("Conexión fallida: " . $con->connect_error);
+// }
+// echo "Conexión exitosa";
+$con= mysqli_connect("localhost", "root", "", "asistencia");
+// if($con){
+//     echo "okey";
+// }else{
+//     echo "error";
+// }
 
 
 ?>
